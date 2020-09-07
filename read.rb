e98c0d035ad99fa40eecfa89eb0f7bff4b343694
ryonakai <nakai@nlab.ci.i.u-tokyo.ac.jp>
2020-09-07 23:56:58 +0900

    initial

--- read.rb
+++ read.rb
@@ -1,8 +1,8 @@
 data = []
 columns = []
 column_names = []
-Dir.glob('data/*').each do |f|
-    d = File.read(f)
+Dir.glob('data/*').each do |fn|
+    d = File.read(fn)
     fam = d.scan(/Model Family:\s+(.*)$/)[0][0] rescue ''
     mod = d.scan(/Device Model:\s+(.*)$/)[0][0] rescue ''
     da = d.scan(/^\s*\d+\s+[A-Z].*$/).map{
@@ -10,7 +10,7 @@ Dir.glob('data/*').each do |f|
         f[0] = f[0].to_i
         f
     }
-    data.push({family: fam, model: mod, data: da})
+    data.push({family: fam, model: mod,  device: fn.split('/').last, data: da})
     columns += da.map{|e| e[0]}#.to_i}
     da.each{|e| 
         column_names[e[0]] = e[1]
@@ -18,31 +18,33 @@ Dir.glob('data/*').each do |f|
     #p d.scan('No Errors Logged')
 end
 columns =  columns.uniq.sort
-p data
-p column_names
-
 txt = ""
 th1 = '<tr><th>Family</th>'
 th2 = '<tr><th>Model</th>'
+th2a = '<tr><th>Device Name</th>'
 th3 = ''
 
 for d in data
     th1 << "<th>#{d[:family]}</th>"
     th2 << "<th>#{d[:model]}</th>"
+    th2a << "<th>#{d[:device]}</th>"
 end
 for c in columns
     th3 <<  "<tr><th>#{c}: #{column_names[c]}</th>"
     for d in data
-        _d = d[:data].select{|e| e[0] == c}[0] rescue ''
+        _d = d[:data].select{|e| e[0] == c}[0] rescue nil
+        _d = _d[9..-1].join(' ') if _d
         th3 << "<td>#{_d}</td>"
     end
     th3 << "</tr>"
 end
 
 File.write('out.html', <<"EOF"
+    <link href="main.css" rel="stylesheet">
     <table>
     #{th1}</tr>
     #{th2}</tr>
+    #{th2a}</tr>
 #{th3}
 </table>
 EOF
